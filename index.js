2bff178b73d57a86e5684129da3c58a4dce9812f
徐志刚 <xuzg@ziroom.com>
2020-10-29 14:40:28 +0800

    [A] add DOOM

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-let str2 = "hello world";
+let str2 = 'hello world';
 let num = 100;
 let num1 = 150;
 let num2 = 200;
 let num3 = 300;
 
+const DOOM = 'dark world';
+
+const fixBug = 'fix it';
 const obj = {name: 'zs', age: 21};
 
 const a = 2;
